2dccdb38192ad2311ae4ab50239f53129a1b5da0
Carly N. Sullivan <carlysullivan2@gmail.com>
2016-11-10 19:05:41 -0500

    jumbotron video holder header radio footer

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// var url = "http://en.wikipedia.org/w/api.php?action=query&titles=Manatee&format=jsonfm"
+
+// //potentially add to url: &origin=https://en.wikipedia.org ???
+
+// var request = {
+// 	url: url,
+// 	method: "GET"
+// }
+
+// $.ajax(request).done(function(response) {
+// 	console.log(response)
+// });
+
+$(document).ready(function() {
+
+$(".manatee-float").fadeIn().animate({width:"300px"}, 1000);	
+
+});
+
